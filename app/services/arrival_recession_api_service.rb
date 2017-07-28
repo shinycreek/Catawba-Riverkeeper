5c741ec3246d10f863134d9e9fd740ceb1c60bea
Haiz Oppenheimer <haizop@gmail.com>
2017-07-28 14:49:14 -0400

    save pairs of arrival recession data

--- app/services/arrival_recession_api_service.rb
+++ app/services/arrival_recession_api_service.rb
@@ -20,24 +20,45 @@ class ArrivalRecessionAPIService
   end
 
   def create_locations_and_arrival_reccessions_for_dam(dam_record, dam_data)
-    dam_data["FlowArrivalRecessions"].map do |arr_rec_data|
-      unless arr_rec_data["MileMarkerName"] == ""
-        location_record = create_arrival_location(dam_record, arr_rec_data)
-        create_arrival_reccession(location_record, arr_rec_data)
-      end
+    get_location_indexes(dam_data).map do |index|
+      location_data_array = get_location_data_by_index(dam_data, index)
+      location_record = create_arrival_location(dam_record, location_data_array)
+      create_arrival_reccessions_for_location(location_record, location_data_array)
     end
   end
 
-  def create_arrival_reccession(location_record, arr_rec_data)
-    location_record.flow_arrival_recessions.where(
-      arrival_time: DateTime.parse(arr_rec_data["Arrival"]),
-      recedes_time: DateTime.parse(arr_rec_data["Recedes"])
+  def get_location_indexes(dam_data)
+    dam_data["FlowArrivalRecessions"].map { |location| location["Index"] }.uniq
+  end
+
+  def get_location_data_by_index(dam_data, index)
+    dam_data["FlowArrivalRecessions"].select do |location_data|
+      location_data["Index"] == index
+    end
+  end
+
+  def create_arrival_location(dam_record, location_data_array)
+    dam_record.flow_arrival_locations.where(
+      name: get_location_name(location_data_array)
     ).first_or_create
   end
 
-  def create_arrival_location(dam_record, arr_rec_data)
-    dam_record.flow_arrival_locations.where(
-      name: arr_rec_data["MileMarkerName"]
+  def get_location_name(location_data_array)
+    location_data_array.map do |location_data|
+      location_data["MileMarkerName"]
+    end.compact[0]
+  end
+
+  def create_arrival_reccessions_for_location(location_record, location_data_array)
+    location_data_array.map do |location_data|
+      create_arrival_reccession(location_record, location_data)
+    end
+  end
+
+  def create_arrival_reccession(location_record, location_data)
+    location_record.flow_arrival_recessions.where(
+      arrival_time: DateTime.parse(location_data["Arrival"]),
+      recedes_time: DateTime.parse(location_data["Recedes"])
     ).first_or_create
   end
 end
